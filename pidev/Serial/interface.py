15256b6143e84bc9760742c5f3b56b73fa210c47
Dino Wang <anguste@gmail.com>
2019-08-18 14:32:25 +1000

    Initial testing code of serial port on RPi

---
+++ pidev/Serial/interface.py
@@ -0,0 +1,17 @@
+import serial
+import time
+
+
+ser = serial.Serial(
+    port='/dev/ttyS0',
+    baudrate=115200,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=1
+)
+counter = 0
+
+while True:
+    x = ser.readline()
+    print(x)
