f6ae98260da25c0f6db7301149d04095f0f5052f
Dino Wang <anguste@gmail.com>
2019-08-19 19:59:55 +1000

    Set up test for RTC

--- demo.py
+++ demo.py
@@ -1,13 +1,21 @@
 import time
-import pidev.RTC.RTC
+import pidev.RTC.RTC as rtc
 
-rtc = pidev.RTC.RTC.Clock()
+clk = rtc.Clock()
 
-print('Setting time to 2013-02-15 16:30:59')
-rtc.set_datetime(user_input=[2013, 2, 15, 16, 30, 59])
+print('Setting time to 2013-02-15 16:30:59 ...')
+clk.set_datetime(user_input=[2013, 2, 15, 16, 30, 59])
 print('Checking time:')
 i = 0
 for i in range(5):
-    print(rtc.get_datetime(output_obj=False))
-    time.sleep(1)
+    print(clk.get_datetime(output_obj=False))
     i += 1
+    time.sleep(1)
+print('Setting time to current system time ...')
+clk.set_datetime()
+print('Checking time:')
+i = 0
+for i in range(5):
+    print(clk.get_datetime(output_obj=False))
+    i += 1
+    time.sleep(1)
