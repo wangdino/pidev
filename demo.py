6dbf3af0fa1fb0fa6da98b6b5e1e4272c1df6789
Dino Wang <anguste@gmail.com>
2019-08-19 18:52:03 +1000

    Optimized datetime handle in RTC interface

--- demo.py
+++ demo.py
@@ -1,4 +1,12 @@
+import time
 import pidev.RTC.RTC
 
 rtc = pidev.RTC.RTC.Clock()
-print(rtc.get_datetime())
+
+print('Setting time to 2013-02-15 16:30:59')
+rtc.set_datetime([2013, 2, 15, 16, 30, 59])
+print('Checking time:')
+i = 0
+for i in range(5):
+    print(rtc.get_datetime(output_obj=False))
+    i += 1
--- pidev/RTC/RTC.py
+++ pidev/RTC/RTC.py
@@ -20,29 +20,60 @@ class Clock:
         self.now = datetime.datetime.now()
         self.weekday = datetime.datetime.now().isoweekday()
 
+    def rtc_encode(self, sys_time):
+        def conv(n):
+            hex_str = hex(int((n - n % 10) / 10 * 16) + n % 10)
+            return int(hex_str, 16)
+        ss = conv(sys_time.second)
+        mm = conv(sys_time.minute)
+        hh = conv(sys_time.hour)
+        day = conv(sys_time.day)
+        month = conv(sys_time.month)
+        year = conv(sys_time.year - 2000)
+        weekday = sys_time.isoweekday()
+        return [ss, mm, hh, weekday, day, month, year]
+
+    def rtc_decode(self, raw_time):
+        ss = int('%02x' % (raw_time[0] & 0x7F))
+        mm = int('%02x' % (raw_time[1] & 0x7F))
+        hh = int('%02x' % (raw_time[2] & 0x3F))
+        # weekday = int((raw_time[3] & 0x07)) - 1
+        day = int('%02x' % (raw_time[4] & 0x3F))
+        month = int('%02x' % (raw_time[5] & 0x1F))
+        year = int('%02x' % (raw_time[6] & 0xFF)) + 2000
+        sys_time = datetime.datetime(year, month, day, hh, mm, ss)
+        return sys_time
+
+    def get_sys_time(self):
+        sys_time = datetime.datetime.now()
+        return sys_time
+
+    def construct_sys_time(self, user_input):
+        # user_input must be an array in form of:
+        # [year, month, day, hour, minute, second]
+        year = user_input[0]
+        month = user_input[1]
+        day = user_input[2]
+        hh = user_input[3]
+        mm = user_input[4]
+        ss = user_input[5]
+        return datetime.datetime(year, month, day, hh, mm, ss)
+
     def set_datetime(self, user_input=None):
         if user_input is None:
-            sys_time = 0  # dummy
-            self.bus.write_i2c_block_data(self.addr, self.reg, sys_time)
+            set_time = self.get_sys_time()
+            self.bus.write_i2c_block_data(self.addr, self.reg, set_time)
         else:
-            try:
-                user_time = 0  # dummy
-                self.bus.write_i2c_block_data(self.addr, self.reg, user_time)
-            except:
-                return 200
-        return 99
+            set_time = self.construct_sys_time(user_input)
+            self.bus.write_i2c_block_data(self.addr, self.reg, set_time)
+        return
 
-    def get_datetime(self):
-        curr_time = self.bus.read_i2c_block_data(self.addr, self.reg, 7)
-        ss = curr_time[0] & 0x7F
-        mm = curr_time[1] & 0x7F
-        hh = curr_time[2] & 0x3F
-        weekday = curr_time[3] & 0x07
-        day = curr_time[4] & 0x3F
-        month = curr_time[5] & 0x1F
-        year = curr_time[6] & 0xFF
-        date_str = str(year) + '-' + str(month) + '-' + str(day)
-        weekday_str = WEEKDAYS[weekday-1]
-        time_str = str(hh) + ':' + str(mm) + ':' + str(ss)
-        curr_time_str = ' '.join((date_str, weekday_str, time_str))
-        return curr_time_str
+    def get_datetime(self, output_obj=True):
+        curr_time_raw = self.bus.read_i2c_block_data(self.addr, self.reg, 7)
+        curr_time = self.rtc_decode(curr_time_raw)
+        if output_obj is True:
+            return curr_time
+        else:
+            weekday = WEEKDAYS[curr_time.weekday()]
+            curr_time_str = curr_time.isoformat(' ') + ' ' + weekday
+            return curr_time_str
